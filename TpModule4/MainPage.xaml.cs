081e0d2b513a2e5198b7bf2f31bb14d110a02358
killian.theophile <Killian.theophile@gmail.com>
2020-06-24 15:54:53 +0200

    TP3

--- TpModule4/MainPage.xaml.cs
+++ TpModule4/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using TpModule4.Models;
 
 namespace TpModule4
 {
@@ -16,6 +17,42 @@ namespace TpModule4
         public MainPage()
         {
             InitializeComponent();
+            new LoginForm(this.pseudo, this.password, this.reminder, this.loginForm, this.tweetForm);
+        }
+        private void Connect_Clicked(object sender, EventArgs e)
+        {
+            if (this.pseudo.Text == null || this.pseudo.Text.Length < 3)
+            {
+                errorHandler("Login non valide");
+            }
+            else if (this.password.Text == null || this.password.Text.Length < 6)
+            {
+                errorHandler("Password non valide");
+            }
+            else
+            {
+                errorHandler(null);
+                switchVue();
+            }
+        }
+
+        private void switchVue()
+        {
+            this.tweetForm.IsVisible = !this.tweetForm.IsVisible;
+            this.loginForm.IsVisible = !this.loginForm.IsVisible;
+        }
+
+        private void errorHandler(String errorText)
+        {
+            if (errorText == null)
+            {
+                this.error.IsVisible = false;
+            }
+            else
+            {
+                this.error.IsVisible = true;
+                this.error.Text = "Password non valide";
+            }
         }
     }
 }
---
+++ TpModule4/Models/LoginForm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace TpModule4.Models
+{
+    public class LoginForm
+    {
+        private ContentView loginForm;
+        private ContentView tweetForm;
+
+
+        // To complete etc...
+       
+      public LoginForm(Entry pseudo, Entry password, Switch reminder, ContentView loginForm, ContentView tweetForm)
+        {
+            this.Login = pseudo;
+            this.Password = password;
+            this.IsReminded = reminder;
+            this.loginForm = loginForm;
+            this.tweetForm = tweetForm;
+        }
+
+        public Entry Login { get; }
+
+        private MainPage Page { get; set; }
+
+        public Entry Password { get; }
+
+        public Switch IsReminded { get; }
+
+        //public VisibilitySwitch VisibilitySwitch { get; }
+
+        //public ErrorForm Error { get; }
+
+       
+
+        private void Connect_Clicked(object sender, EventArgs e)
+        {
+            if (Login.Text == null)
+            {
+               // Error.text = "";
+                Console.WriteLine("IHIHIHIHIHIHI");
+            }
+
+            if (Password.Text == null)
+            {
+                Console.WriteLine("OHOHOHOH");
+            }
+
+            
+        }
+    }
+}
